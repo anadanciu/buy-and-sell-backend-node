a0bc5ece4a60c2517074c9a93d9f0b5ff576ed60
anadanciu <anamaria.danciu.ad@gmail.com>
2023-07-22 19:15:20 +0300

    correcting sql string

--- src/routes/createNewListing.js
+++ src/routes/createNewListing.js
@@ -1,19 +1,25 @@
-import { v4 as uuid } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { db } from "../database";
 
 export const createNewListingRoute = {
   method: "POST",
   path: "/api/listings",
   handler: async (req, h) => {
-    const id = uuid();
+    const id = uuidv4();
     const { name = "", description = "", price = 0 } = req.payload;
     const userId = "12345";
     const views = 0;
 
-    await db.query(
-      `INSERT INTO listings (id, name, description, price, user_id, views)
-    VALUES (? ? ? ? ? ?)`[(id, name, description, price, userId, views)]
-    );
+    console.warn(id, name, description, price, userId, views);
+    try {
+      await db.query(
+        `INSERT INTO listings (id, name, description, price, user_id, views)
+    VALUES (?, ?, ?, ?, ?, ?)`,
+        [id, name, description, price, userId, views]
+      );
+    } catch (error) {
+      console.warn(error);
+    }
 
     return { id, name, description, price, user_id: userId, views };
   },
